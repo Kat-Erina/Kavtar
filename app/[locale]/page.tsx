08304b0c8c5737c47da116b4d34cefc2c87b7af3
Kat-Erina <nalchevanidzekaterina@gmail.com>
2026-06-03 11:23:12 +0400

    added metadata

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,6 +6,52 @@ import CorePillars from '@/components/CorePillars'
 import Projects from '@/components/projects/Projects'
 import { Contact } from '@/components/Contact'
 import AnimatedComponent from '@/components/AnimatedComponent'
+import { getTranslations } from 'next-intl/server'
+
+
+
+
+export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params
+  const t = await getTranslations({ locale, namespace: 'metadata' })
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    keywords: t('keywords').split(',').map(k => k.trim()),
+    openGraph: {
+      title: t('ogTitle'),
+      description: t('ogDescription'),
+      type: "website",
+     siteName: "Kavtar Development",
+    },
+    robots: {
+      index: true,
+      follow: true,
+    },
+    alternates: {
+      canonical: '/',
+      languages: {
+        'en': '/en',
+        'ka': '/ka'
+      }
+    },
+
+      appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "Kavtar Development",
+  },
+    
+  }
+}
+
+
+
+
+
+
+
 const page =async ({ params }: { params: Promise<{ locale: string }> }) => {
   const {locale}=await params;
   return (
